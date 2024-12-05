9dedd2b2c512343fa8b151bcda11d96e77a34a80
Christina Takara <christina.takara@gmail.com>
2024-12-05 13:17:58 -0600

    added fruit model

---
+++ models/fruits.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const fruitSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    color: {type: String, required: true},
+    readyToEat: Boolean
+}, {
+    timestamps: true
+})
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+module.exports = Fruit;
